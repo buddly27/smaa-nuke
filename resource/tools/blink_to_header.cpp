0f7e9aa927edda8c6b0310fbd941d2373340614a
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2019-01-01 22:38:50 +0100

    Update license info in blink and tool codes

--- resource/tools/blink_to_header.cpp
+++ resource/tools/blink_to_header.cpp
@@ -1,5 +1,8 @@
 /**
  * Copyright (C) 2019, Jeremy Retailleau
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #include <algorithm>
